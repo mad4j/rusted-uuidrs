a6fad5d84e7d3c5357225536645e2529501c3b4a
Daniele Olmisani <daniele.olmisani@gmail.com>
2021-11-13 14:48:07 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,57 @@
-
 mod uuid;
 
-use uuid::UUIDType;
+use std::str::FromStr;
 use structopt::StructOpt;
+use uuid::UUIDType;
+
+#[derive(Debug)]
+enum OutputFormat {
+    STR,
+    SIV,
+}
+
+impl FromStr for OutputFormat {
+    type Err = i32;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().trim() {
+            "str" => Ok(OutputFormat::STR),
+            "siv" => Ok(OutputFormat::SIV),
+            _ => Err(-1),
+        }
+    }
+}
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "uuidrs", about = "geration of UUID values")]
+#[structopt(
+    name = "uuidrs",
+    about = "Universally Unique Identifier Command-Line Tool in Rust",
+    author = "daniele.olmisani@gmail.com",
+)]
 struct Opt {
     /// number of ids to be generated
     #[structopt(short, long, default_value = "1")]
     count: u16,
+
+    /// format of generated ids
+    #[structopt(short, long, default_value = "str")]
+    format: OutputFormat,
 }
 
 fn main() {
-
     // parse command-line parameters
     let opt = Opt::from_args();
 
     for _ in 0..opt.count {
-        println!("{}", UUIDType::random());
+
+        // generate a new id
+        let uiid = UUIDType::random();
+
+        println!(
+            "{}",
+            match opt.format {
+                OutputFormat::STR => uiid.to_string(),
+                OutputFormat::SIV => uiid.to_siv_string(),
+            }
+        );
     }
 }
--- src/uuid.rs
+++ src/uuid.rs
@@ -1,32 +1,50 @@
 use rand::Rng;
 use std::fmt;
 
+
+/// 0                   1                   2                   3
+/// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |                          time_low                             |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |       time_mid                |         time_hi_and_version   |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |clk_seq_hi_res |  clk_seq_low  |         node (0-1)            |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |                         node (2-5)                            |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 #[derive(Debug)]
 pub struct UUIDType {
-    //xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
+    // xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
     value: [u8; 16],
 }
-
+/*
 impl Into<[u8; 16]> for UUIDType {
     fn into(self) -> [u8; 16] {
         self.value
     }
 }
-
+*/
 impl UUIDType {
     pub fn random() -> Self {
-        //random bits
+        // random bits
         let mut value = rand::thread_rng().gen::<[u8; 16]>();
 
-        //fix version 4
+        // fix version 4
         value[6] &= 0b0000_1111;
         value[6] |= 0b0100_0000;
 
-        //fix variant 1
+        // fix variant 1
         value[8] &= 0b0011_1111;
         value[8] |= 0b1000_0000;
 
+        // return generated value
         UUIDType { value }
+    }
+
+    pub fn to_siv_string(&self) -> String {
+        u128::from_be_bytes(self.value).to_string()
     }
 }
 
